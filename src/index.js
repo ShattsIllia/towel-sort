08a5b2a96f09c1737d1efe63891e59a2dc05b95d
ShattsIllia <shatts.ilya@gmail.com>
2020-10-23 14:54:38 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
-
 // You should implement your task here.
+let generalArray;
+module.exports = function towelSort(matrix) {
+    if (matrix === undefined || matrix.length === 0) {
+        return [];
+    }
+    
+    if (matrix.length === 2) {
+        generalArray = matrix[0].concat(matrix[1].reverse());
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+    }
+    if (matrix.length === 3) {
+        generalArray = matrix[0].concat(matrix[1].reverse(), matrix[2]);
+
+    }
+    if (matrix.length === 4) {
+        generalArray = matrix[0].concat(matrix[1].reverse(), matrix[2], matrix[3].reverse());
+
+    }
+    return generalArray;
+};
+
